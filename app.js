6cca12f8d494883151a2b605734f02944f7955d7
JuniorVitor <junior.vitorcc@gmail.com>
2018-08-02 21:36:22 -0300

    Teste router

--- app.js
+++ app.js
@@ -20,12 +20,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+/*app.use('/bios', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
-});
+});*/
 
 // error handler
 app.use(function(err, req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Fist App', status: 'Happy'});
 });
+
+/* GET test page. */
+router.get('/helicopterson', function(req, res, next){
+  res.render('index', { title: 'Teste', status: 'testante'});
+})
+
 
 module.exports = router;
